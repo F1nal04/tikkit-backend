e70f3234d7486022e447337d37691aa30ba02aee
Leon Bojanowski <leongaborbojanowski04@gmail.com>
2025-05-17 16:00:59 +0200

    updated models

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 from sqlalchemy import UUID, Boolean, Column, ForeignKey, String, Text, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from schemas import Topic, Status, Priority, Role
 
@@ -22,10 +22,21 @@ class Ticket(Base):
     priority: Mapped[Priority] = mapped_column(
         nullable=False)
     assigned_to = Column(UUID, ForeignKey("users.id"), nullable=True)
+    assigned_to_user = relationship(
+        "User", foreign_keys=[assigned_to], nullable=True)
     author = Column(UUID, ForeignKey("users.id"), nullable=False)
+    author_user = relationship("User", foreign_keys=[author], nullable=False)
     created_at = Column(Date, default=date.today)
     updated_at = Column(Date, default=date.today,
                         onupdate=date.today)
+
+    @property
+    def author_name(self):
+        return self.author_user.name
+
+    @property
+    def assigned_to_name(self):
+        return self.assigned_to_user.name if self.assigned_to_user else None
 
 
 class User(Base):
@@ -36,6 +47,7 @@ class User(Base):
     name = Column(String, index=True, nullable=False)
     hashed_password = Column(String(128), nullable=False)
     role: Mapped[Role] = mapped_column(default=Role.user)
+    tickets = relationship("Ticket", back_populates="author_user")
     created_at = Column(Date, default=date.today)
     updated_at = Column(Date, default=date.today,
                         onupdate=date.today)
